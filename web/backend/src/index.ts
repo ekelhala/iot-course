92deb264864028ae8349c69201a6eb5627999d11
Emil Kelhälä <emil.kelhala@gmail.com>
2024-11-23 18:05:47 +0200

    Disabled X-Powered-By header, added custom 404 response in JSON

--- web/backend/src/index.ts
+++ web/backend/src/index.ts
@@ -8,12 +8,18 @@ const app = Express();
 
 // App-wide settings
 app.use(Express.json());
+app.disable('x-powered-by');
 
 // Attach routers from their respective modules
 app.use('/sensors', sensors);
 
 app.get('/', (req, res) => {
     res.json({data: 'Server is up!'});
+})
+
+// Not found
+app.use((req, res, next) => {
+    res.status(404).json({error: 'Not found'});
 })
 
 app.listen(PORT, () => {
