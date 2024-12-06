1e07cccfbada2e3fdd8dc6acd83afeb8f1bf7f1d
tpantsar <tomi.pantsar@gmail.com>
2024-12-06 14:41:50 +0200

    web/weather-collector - add prettier config and format files

--- web/weather-collector/models/WeatherData.ts
+++ web/weather-collector/models/WeatherData.ts
@@ -1,20 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const weatherDataSchema = new mongoose.Schema({
+const weatherDataSchema = new mongoose.Schema(
+  {
     value: Number,
-    timestamp: Date
-},
-{
+    timestamp: Date,
+  },
+  {
     toJSON: {
-        transform: (doc, ret) => {
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        }
-    }
-});
+      transform: (doc, ret) => {
+        delete ret._id
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)
 
-export const TemperatureIn = mongoose.model('TemperatureIn', weatherDataSchema);
-export const TemperatureOut = mongoose.model('TemperatureOut', weatherDataSchema);
-export const Humidity = mongoose.model('Humidity', weatherDataSchema);
-export const Pressure = mongoose.model('Pressure', weatherDataSchema);
+export const TemperatureIn = mongoose.model('TemperatureIn', weatherDataSchema)
+export const TemperatureOut = mongoose.model('TemperatureOut', weatherDataSchema)
+export const Humidity = mongoose.model('Humidity', weatherDataSchema)
+export const Pressure = mongoose.model('Pressure', weatherDataSchema)
