b856dcd332e41795a8e22988d3d283b51b4c012a
Emil Kelhälä <emil.kelhala@gmail.com>
2024-12-13 10:21:54 +0200

    frontend - add Recharts, create WeatherDataGraph

---
+++ web/frontend/src/components/WeatherDataGraph.tsx
@@ -0,0 +1,8 @@
+import { LineChart } from 'recharts'
+import WeatherDataPoint from '../types/WeatherDataPoint'
+
+const WeatherDataGraph = (props: { data: WeatherDataPoint[] }) => {
+  return <LineChart></LineChart>
+}
+
+export default WeatherDataGraph
