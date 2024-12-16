30e028da66f75442aee4d9f3160e5620b8b3db81
tpantsar <tomi.pantsar@gmail.com>
2024-12-16 15:33:31 +0200

    Create basic table on LatestWeatherTable

--- web/frontend/src/components/LatestWeatherTable.tsx
+++ web/frontend/src/components/LatestWeatherTable.tsx
@@ -1,4 +1,10 @@
-import { Box } from '@mui/material'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
 import WeatherData from '../types/WeatherData'
 
 interface LatestWeatherTableProps {
@@ -7,29 +13,36 @@ interface LatestWeatherTableProps {
 
 const LatestWeatherTable = ({ weatherDataLatest }: LatestWeatherTableProps) => {
   return (
-    <Box>
-      <b>Latest weather data</b>
-      <table className="weather-table">
-        <tbody>
-          <tr>
-            <td>temperature_in</td>
-            <td>{weatherDataLatest.temperature_in?.value}&deg;C</td>
-          </tr>
-          <tr>
-            <td>temperature_out</td>
-            <td>{weatherDataLatest.temperature_out?.value}&deg;C</td>
-          </tr>
-          <tr>
-            <td>humidity</td>
-            <td>{weatherDataLatest.humidity?.value}%</td>
-          </tr>
-          <tr>
-            <td>pressure</td>
-            <td>{weatherDataLatest.pressure?.value} hPa</td>
-          </tr>
-        </tbody>
-      </table>
-    </Box>
+    <TableContainer className="weather-table" component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Type</TableCell>
+            <TableCell align="left">Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              temperature_in
+            </TableCell>
+            <TableCell align="left">{weatherDataLatest.temperature_in?.value}&deg;C</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>temperature_out</TableCell>
+            <TableCell align="left">{weatherDataLatest.temperature_out?.value}&deg;C</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>humidity</TableCell>
+            <TableCell align="left">{weatherDataLatest.humidity?.value} %</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>pressure</TableCell>
+            <TableCell align="left">{weatherDataLatest.pressure?.value} hPa</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
